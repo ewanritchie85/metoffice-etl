552a1a5d9591b50b55795523f520a42e7921a97f
EWAN <ewanritchie@hotmail.co.uk>
2025-06-08 17:00:19 +0100

    get hourly forecast function added to api.py along with unit test

--- api/api.py
+++ api/api.py
@@ -1,8 +1,36 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+from pprint import pprint
+import os
+import requests
 
+load_dotenv()
+API_KEY = os.getenv("METOFFICE_API_KEY")
 
 app = FastAPI()
 
+
 @app.get("/health")
 async def health_check():
+    """health check endpoint
+
+    Returns:
+        dict: A dictionary with the status of the API.
+    """
     return {"status": "ok"}
+
+
+@app.get("/sitespecific/v0/point/hourly")
+async def get_hourly_data(latitude: float, longitude: float):
+
+    url = "https://data.hub.api.metoffice.gov.uk/sitespecific/v0/point/hourly"
+    headers = {"accept": "application/json", "apikey": os.getenv("METOFFICE_API_KEY")}
+    params = {
+        "latitude": latitude,
+        "longitude": longitude,
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+    response.raise_for_status()
+    pprint(response.json())
+    return response.json()
--- test/test_api.py
+++ test/test_api.py
@@ -1,15 +1,32 @@
 from fastapi.testclient import TestClient
-from api.api import app
 import pytest
+from api.api import app
 
 
 @pytest.fixture
 def client():
     return TestClient(app)
 
+@pytest.fixture
+def test_response(client):
+    test_latitude = 57.149651
+    test_longitude = -2.099075
+    test_response = client.get(
+            f"/sitespecific/v0/point/hourly?latitude={test_latitude}&longitude={test_longitude}"
+        )
+    return test_response
 
-def test_api_health(client):
-    response = client.get("/health")
-    assert response.status_code == 200
-    assert response.json() == {"status": "ok"}
-    assert response.headers["Content-Type"] == "application/json"
+
+class TestHealthCheck:
+    def test_api_health(self, client):
+        response = client.get("/health")
+        assert response.status_code == 200
+        assert response.json() == {"status": "ok"}
+        assert response.headers["Content-Type"] == "application/json"
+
+
+class TestGetHourlyData:
+    def test_get_hourly_data(self, test_response):
+
+        assert test_response.status_code == 200
+        assert test_response.headers["Content-Type"] == "application/json"
