4d84b318ee52b0d10ba522e01fae95fe59492a94
EWAN <ewanritchie@hotmail.co.uk>
2025-06-08 22:55:45 +0100

    upload json to landing s3 function written, and logging added to extract.py

--- src/extract/extract.py
+++ src/extract/extract.py
@@ -1,4 +1,16 @@
+from datetime import datetime
+import json
+import boto3
+import logging
+import os
 from api.api import get_forecast_data
+
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
+logger = logging.getLogger(__name__)
 
 
 def get_data_from_api(span: str, city: str) -> dict:
@@ -12,14 +24,39 @@ def get_data_from_api(span: str, city: str) -> dict:
     Returns:
         dict: The JSON response from the API.
     """
-    print("getting_data_from_api called")
+    logger.info(f"fetching forecast data for {city} with span {span}")
     return get_forecast_data(span, city)
 
-def upload_json_to_landing_s3() -> None:
+
+def upload_json_to_landing_s3(span: str, city: str, bucket=None, s3_client=None) -> str:
+    """Uploads forecast data JSON to landing S3 bucket.
+
+    Args:
+        span (str): daily / hourly / three-hourly time span for the data.
+        city (str): name of the city.
+        bucket (str): name of the S3 bucket to upload to.
+        s3_client (_type_, optional): Defaults to None, but is created if not provided.
+
+    Returns:
+        str: _description_
     """
-    Upload the JSON data to the landing S3 bucket.
-    This function is a placeholder and should be implemented.
-    """
-    print("upload_json_to_landing_s3 called")
-    # Implementation goes here
-    pass
+    logger.info(f"uploading forecast data for {city} with span {span} to S3 bucket {bucket}")
+    if not s3_client:
+        s3_client = boto3.client("s3")
+    if not bucket:
+        bucket = os.getenv("LANDING_BUCKET_NAME")
+
+    date = datetime.now()
+    date_str = date.strftime("%Y/%m/%d/%H-%M")
+
+    forecast_data = None
+    try:
+        forecast_data = get_data_from_api(span, city)
+    except (ConnectionError, TimeoutError) as e:
+        logger.error(f"Error fetching data from API: {e}")
+        forecast_data = {"error": "Failed to fetch data from API"}
+
+    s3_client.put_object(
+        Bucket=bucket, Key=f"{city}/{date_str}.json", Body=json.dumps(forecast_data)
+    )
+    return f"{city}/{date_str}.json"
