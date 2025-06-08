ccbf08135eccde0db1fbd336743a9bb10e9e4c92
EWAN <ewanritchie@hotmail.co.uk>
2025-06-08 18:05:11 +0100

    utility function added to get latitude and longitude from city name, along with unit tests

---
+++ test/test_utils.py
@@ -0,0 +1,29 @@
+import pytest
+
+from utils.utils import get_lat_long_from_city
+
+
+class TestGetLatLong:
+    def test_get_lat_long_from_real_city(self):
+        test_city = "Tokyo"
+        expected_lat_long = (35.6870, 139.7495)
+        lat_long = get_lat_long_from_city(test_city)
+        assert lat_long == expected_lat_long
+
+    def test_get_lat_long_from_non_existent_city(self):
+        test_city = "NonExistentCity"
+        with pytest.raises(ValueError) as e:
+            get_lat_long_from_city(test_city)
+        assert str(e.value) == "'NonExistentCity' not found in the dataset."
+        
+    def test_get_lat_long_from_city_with_extra_spaces(self):
+        test_city = "  London  "
+        expected_lat_long = (51.5072, -0.1275)
+        lat_long = get_lat_long_from_city(test_city)
+        assert lat_long == expected_lat_long
+        
+    def test_get_lat_long_from_non_ascii_city_(self):
+        test_city = "São Paulo"
+        expected_lat_long = (-23.5504, -46.6339)
+        lat_long = get_lat_long_from_city(test_city)
+        assert lat_long == expected_lat_long
---
+++ utils/utils.py
@@ -0,0 +1,23 @@
+import csv
+
+def get_lat_long_from_city(city: str) -> tuple:
+    """
+    Get latitude and longitude from a city name.
+
+    Args:
+        city (str): The name of the city.
+
+    Returns:
+        tuple: A tuple containing latitude and longitude.
+    """
+    with open('data/world-cities.csv', mode='r', encoding="utf-8") as file:
+        
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            if row['city'].strip().lower() == city.strip().lower():
+                lat_long = (float(row['latitude']), float(row['longitude']))
+                break
+        else:
+            raise ValueError(f"'{city}' not found in the dataset.")
+        
+    return lat_long
