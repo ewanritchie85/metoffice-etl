1e983b735a4bedaaf01253d45f6857eef4fc7ba4
EWAN <ewanritchie@hotmail.co.uk>
2025-06-10 15:58:10 +0100

    try/except added to transform logic

--- src/transform/transform.py
+++ src/transform/transform.py
@@ -20,27 +20,33 @@ def transform_data_to_dataframe(bucket=None, s3_client=None) -> list:
     dfs = []
     bucket_objects = s3_client.list_objects_v2(Bucket=bucket)['Contents']
     for object in bucket_objects:
-        key = object['Key']
-        city = key.split('/')[-1].replace('.json', '')
-        logger.info(f"Retrieving S3 object: {key}")
-        response = s3_client.get_object(Bucket=bucket, Key=key)
+        try:
+            key = object['Key']
+            if not key or not key.endswith('.json'):
+                continue
+            city = key.split('/')[-1].replace('.json', '')
+            logger.info(f"Retrieving S3 object: {key}")
+            response = s3_client.get_object(Bucket=bucket, Key=key)
 
-        logger.info(f"Reading {key}")
-        json_data = json.loads(response["Body"].read().decode("utf-8"))
+            logger.info(f"Reading {key}")
+            json_data = json.loads(response["Body"].read().decode("utf-8"))
 
-        forecasts = json_data["features"][0]["properties"]["timeSeries"]
-        coordinates = json_data["features"][0]["geometry"]["coordinates"]
+            forecasts = json_data["features"][0]["properties"]["timeSeries"]
+            coordinates = json_data["features"][0]["geometry"]["coordinates"]
 
-        df = pd.DataFrame(forecasts)
-        df = select_wanted_columns(df)
-        df.insert(0, "City", city)
-        df["Latitude"] = coordinates[1]
-        df["Longitude"] = coordinates[0]
-        df["Elevation"] = coordinates[2]
-        dfs.append(df)
-        df.to_csv("data/test_csv.csv", index=False)
+            df = pd.DataFrame(forecasts)
+            df = select_wanted_columns(df)
+            df.insert(0, "City", city)
+            df["Latitude"] = coordinates[1]
+            df["Longitude"] = coordinates[0]
+            df["Elevation"] = coordinates[2]
+            dfs.append(df)
+        except (KeyError, json.JSONDecodeError) as e:
+            logger.warning(f"skipping {key} due to: {e}") # type: ignore
+            continue
     pprint(dfs)
     return dfs
+
 
 
 # temporary main function to test transform
--- terraform/db.tf
+++ terraform/db.tf
@@ -1,10 +1,10 @@
-# resource "aws_db_instance" "loading_db" {
-#   db_name           = var.loading_db_name
-#   instance_class    = "db.t3.micro"
-#   allocated_storage = 10
-#   engine            = "mysql"
-#   engine_version    = "8.0"
-#   username  = var.loading_db_username
-#   password = var.loading_db_password
+resource "aws_db_instance" "loading_db" {
+  db_name           = var.loading_db_name
+  instance_class    = "db.t3.micro"
+  allocated_storage = 10
+  engine            = "mysql"
+  engine_version    = "8.0"
+  username  = var.loading_db_username
+  password = var.loading_db_password
 
-# }
+}
