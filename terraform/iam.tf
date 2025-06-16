e6f99b50f46d1505f641440e1340a95149f9c25f
EWAN <ewanritchie@hotmail.co.uk>
2025-06-16 16:24:59 +0100

    ecs task role iam policy added

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -81,3 +81,22 @@ resource "aws_iam_role_policy" "ecs_run_task_policy" {
     }]
   })
 }
+
+resource "aws_iam_role" "ecs_task_role" {
+  name = "metoffice-etl-task-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "s3_access" {
+  role       = aws_iam_role.ecs_task_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess" 
+}
