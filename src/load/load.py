de2b63f25172601461ad430317646de4b5f3f400
EWAN <ewanritchie@hotmail.co.uk>
2025-06-18 10:42:38 +0100

    logic added to load function to ensuire table exists/create table along with a try except and some loggers

--- src/load/load.py
+++ src/load/load.py
@@ -2,6 +2,7 @@ from typing import List
 import pandas as pd
 from dotenv import load_dotenv
 from utils.utils import setup_logger
+from db.db import create_db_table
 import os
 from sqlalchemy import create_engine, text
 
@@ -11,31 +12,43 @@ if os.getenv("RUNNING_IN_ECS") != "true":
     load_dotenv()
 
 
-# Insert transformed forecast data into RDS using pandas and sqlalchemy
 def insert_forecasts_to_db(dfs: List[pd.DataFrame]) -> None:
+
     engine = create_engine(
         f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@"
         f"{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
     )
-    with engine.connect() as conn:
-        result = conn.execute(text("SELECT City, forecast_time FROM weather_forecast"))
-        existing_keys = result.fetchall()
-        existing_set = set((row[0], row[1]) for row in existing_keys)
-        existing_set = set(
-            (city, forecast_time.replace(tzinfo=None))
-            for city, forecast_time in existing_set
-        )
+    logger.info("Ensuring database table exists")
+    create_db_table()
 
-    for df in dfs:
-        df["forecast_time"] = pd.to_datetime(df["forecast_time"]).dt.tz_localize(None)
-        df_filtered = df[
-            ~df.apply(
-                lambda row: (row["City"], row["forecast_time"]) in existing_set, axis=1
+    try:
+        with engine.connect() as conn:
+            result = conn.execute(
+                text("SELECT City, forecast_time FROM weather_forecast")
             )
-        ]
-        if not df_filtered.empty:
-            df_filtered.to_sql(
-                "weather_forecast", con=engine, if_exists="append", index=False
+            existing_keys = result.fetchall()
+            existing_set = set((row[0], row[1]) for row in existing_keys)
+            existing_set = set(
+                (city, forecast_time.replace(tzinfo=None))
+                for city, forecast_time in existing_set
             )
 
-    logger.info("Forecast data inserted into RDS via pandas.to_sql.")
+        for df in dfs:
+            df["forecast_time"] = pd.to_datetime(df["forecast_time"]).dt.tz_localize(
+                None
+            )
+            df_filtered = df[
+                ~df.apply(
+                    lambda row: (row["City"], row["forecast_time"]) in existing_set,
+                    axis=1,
+                )
+            ]
+            if not df_filtered.empty:
+                df_filtered.to_sql(
+                    "weather_forecast", con=engine, if_exists="append", index=False
+                )
+
+            logger.info("Forecast data inserted into RDS via pandas.to_sql.")
+    except Exception as e:
+        logger.error(f"Error inserting data into RDS: {str(e)}")
+        raise
