fe426e6ee60c16c1f62b6f6214d0eee57e69b484
EWAN <ewanritchie@hotmail.co.uk>
2025-06-18 10:05:44 +0100

    security groups tightened up in tf file

--- terraform/securitygroups.tf
+++ terraform/securitygroups.tf
@@ -1,23 +1,37 @@
-
-
-
 resource "aws_security_group" "ecs_service_sg" {
   name        = "ecs-service-sg"
-  description = "Allow ECS tasks to communicate and access RDS and internet"
+  description = "Security group for ECS tasks"
   vpc_id      = aws_vpc.main.id
 
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
+  # Allow HTTPS outbound (for Met Office API)
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Allow all MySQL outbound (will be restricted by RDS security group)
+  egress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "rds_sg" {
+  name        = "rds-sg"
+  description = "Security group for RDS instance"
+  vpc_id      = aws_vpc.main.id
+}
+
+# Add rules after security group creation
+resource "aws_security_group_rule" "rds_ingress" {
+  type                     = "ingress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.ecs_service_sg.id
+  security_group_id        = aws_security_group.rds_sg.id
 }
